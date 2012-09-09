7efce7bdf296a91de5e68312482a278aed67558a
Seth Buntin <seth.buntin@gmail.com>
2012-09-09 13:15:27 -0500

    add tomorrow-night theme

--- init.el
+++ init.el
@@ -3,17 +3,21 @@
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
+;; set theme dir and tomorrow-night theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'tomorrow-night t)
+
 ;; desired starter-kit packages
 (when (not package-archive-contents)
   (package-refresh-contents))
 
 (defvar my-packages '(starter-kit starter-kit-lisp starter-kit-eshell
                                   starter-kit-js starter-kit-bindings
-                                  clojure-mode clojure-test-mode markdown-mode zenburn-theme)
+                                  clojure-mode clojure-test-mode markdown-mode)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
 
-(eshell)
+;; (eshell)
