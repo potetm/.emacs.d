18a1351e686fd57a53546b0f5fb8b4f9b5792af2
Seth Buntin <seth.buntin@gmail.com>
2013-04-05 13:15:53 -0500

    set theme to wombat and turn off line highlight

--- init.el
+++ init.el
@@ -6,6 +6,8 @@
 ;; add themes folder to theme load path
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 
+(load-theme 'wombat t)
+
 ;; desired starter-kit packages
 (when (not package-archive-contents)
   (package-refresh-contents))
@@ -69,3 +71,25 @@
      (add-to-list 'grep-find-ignored-directories "migrations")
      (add-to-list 'grep-find-ignored-directories "target")
      (add-to-list 'grep-find-ignored-directories "out")))
+
+;; Teach compile the syntax of the kibit output
+(require 'compile)
+(add-to-list 'compilation-error-regexp-alist-alist
+         '(kibit "At \\([^:]+\\):\\([[:digit:]]+\\):" 1 2 nil 0))
+(add-to-list 'compilation-error-regexp-alist 'kibit)
+
+;; A convenient command to run "lein kibit" in the project to which
+;; the current emacs buffer belongs to.
+(defun kibit ()
+  "Run kibit on the current project.
+Display the results in a hyperlinked *compilation* buffer."
+  (interactive)
+  (compile "lein kibit"))
+
+(defun kibit-current-file ()
+  "Run kibit on the current file.
+Display the results in a hyperlinked *compilation* buffer."
+  (interactive)
+  (compile (concat "lein kibit " buffer-file-name)))
+
+(remove-hook 'prog-mode-hook 'esk-turn-on-hl-line-mode)
