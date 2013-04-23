8b4dd077da9f156eb5275c542b309d7b08583d57
Seth Buntin <seth.buntin@gmail.com>
2013-04-23 10:08:28 -0500

    remove unused user folder and bump font size

--- init.el
+++ init.el
@@ -46,7 +46,7 @@
 (set-face-background 'vertical-border "white")
 
 ;; font size
-(set-face-attribute 'default nil :font "Droid Sans Mono for Powerline" :height 120)
+(set-face-attribute 'default nil :font "Droid Sans Mono for Powerline" :height 150)
 
 ;; markdown
 ;; add file extentions to mode auto load
