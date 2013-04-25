250e486caf9bd005959bc13e4c0510d6b3c92ad3
Seth Buntin <seth.buntin@gmail.com>
2013-04-25 09:19:21 -0500

    more simple mouse support for terminal

--- init.el
+++ init.el
@@ -27,17 +27,10 @@
 (setq projectile-globally-ignored-files '("TAGS" "*.js"))
 
 ;; Enable mouse support
-(unless window-system
-  (require 'mouse)
-  (xterm-mouse-mode t)
-  (global-set-key [mouse-4] '(lambda ()
-                               (interactive)
-                               (scroll-down 1)))
-  (global-set-key [mouse-5] '(lambda ()
-                               (interactive)
-                               (scroll-up 1)))
-  (defun track-mouse (e))
-  (setq mouse-sel-mode t))
+(require 'mouse)
+(xterm-mouse-mode t)
+(defun track-mouse (e))
+(setq mouse-sel-mode t)
 
 ;; (eshell)
 
