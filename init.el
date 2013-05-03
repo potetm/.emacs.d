2410a941b362afc04be775ca139907c3d4f59838
Seth Buntin <seth.buntin@gmail.com>
2013-05-03 10:59:25 -0500

    only use melpa

--- init.el
+++ init.el
@@ -1,7 +1,5 @@
 (require 'package)
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (package-initialize)
 
